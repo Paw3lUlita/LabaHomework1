3c389f32ab21d9039c9f8c8d18856a890bb14f7e
Paw3lUlita <pawel.ulita33@gmail.com>
2022-11-17 09:38:24 +0100

    insertion + selection tests

--- src/test/java/org/example/Homework1Test.java
+++ src/test/java/org/example/Homework1Test.java
@@ -20,10 +20,28 @@ class Homework1Test {
     }
 
     @Test
-    void selectionSort() {
+    void selectionSortTest() {
+        //given
+        int[] testArr = {1, 4, 3, 7, 78, 5};
+        int[] expected =  {1, 3, 4, 5, 7, 78};
+
+        //when
+        int[] result = Homework1.selectionSort(testArr);
+
+        //then
+        assertArrayEquals(expected, result);
     }
 
     @Test
-    void insertionSort() {
+    void insertionSortTest() {
+        //given
+        int[] testArr = {99, 87, 56, 12, 234, 6};
+        int[] expected =  {6, 12, 56, 87, 99, 234};
+
+        //when
+        int[] result = Homework1.insertionSort(testArr);
+
+        //then
+        assertArrayEquals(expected, result);
     }
 }
